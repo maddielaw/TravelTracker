9f55deaf8a785dcad8022998e469b56b4ddfc287
Madison Law <madisonlaw1@gmail.com>
2022-03-01 15:11:10 -0700

    Add initial setup for Traveler class and test

--- test/Traveler-test.js
+++ test/Traveler-test.js
@@ -4,9 +4,41 @@ import Traveler from '../src/Traveler';
 
 describe('Traveler', () => {
 
+  let travelerData, travelerTrips, traveler;
 
   beforeEach(function() {
 
+    travelerData = {
+        id: 1,
+        name: "Ham Leadbeater",
+        travelerType: "relaxer",
+    };
+
+    travelerTrips = [
+        {id: 117, 
+        userID: 1, 
+        destinationID: 28, 
+        travelers: 3, 
+        date: "2021/01/09", 
+        duration: 15, 
+        status: "approved", 
+        suggestedActivities: []}
+      ];
+    
+    traveler = new Traveler(travelerData, travelerTrips)
+
+  })
+
+  it('should be a function', function () {
+    expect(Traveler).to.be.a('function');
+  });
+
+  it('should instantiate a TravelDatabase', function () {
+    expect(traveler).to.be.an.instanceof(Traveler);
+  });
+
+  it('should keep track of a traveler\'s id', function () {
+    expect(traveler.id).to.equal(1)
   })
 
 
