2db91b1121e6e75374066843e28023c12026bb9f
Madison Law <madisonlaw1@gmail.com>
2022-03-01 14:08:19 -0700

    Add class & test files

---
+++ test/TravelDatabase-test.js
@@ -0,0 +1,19 @@
+import { expect } from 'chai';
+import TravelDatabase from '../src/TravelDatabase';
+
+
+describe('Travel Database', () => {
+
+  
+  beforeEach(function() {
+
+
+  })
+
+
+
+
+
+
+
+})
