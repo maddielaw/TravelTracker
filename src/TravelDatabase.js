e36f2bafdb161e9cb6af532d8cff44860a03b3f3
Madison Law <madisonlaw1@gmail.com>
2022-03-01 14:34:27 -0700

    Add test for tracking all travel data and travelers

--- src/TravelDatabase.js
+++ src/TravelDatabase.js
@@ -3,9 +3,19 @@
 class TravelDatabase {
   constructor(data){
     this.data = data;
-
+    this.travelers = data.travelers;
   }
 }
 
 
 export default TravelDatabase;
+
+
+
+
+/*
+this.travelerData = data.travelerData;
+    this.tripsData = data.tripsData;
+    this.destinationsData = data.destinationsData;
+    this.currentTraveler = {};
+*/
