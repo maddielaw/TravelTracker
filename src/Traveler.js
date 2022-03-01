629666959eaeba6c6c5119d8a63a5c5101f995d3
Madison Law <madisonlaw1@gmail.com>
2022-03-01 16:07:46 -0700

    Add function & test to sort a traveler's trips by date

--- src/Traveler.js
+++ src/Traveler.js
@@ -5,7 +5,21 @@ class Traveler {
     this.name = travelerData.name;
     this.travelerType = travelerData.travelerType;
     this.travelerTrips = travelerTrips;
+    // this.pastTrips = [];
+    // this.upcomingTrips = [];
+    // this.pendingTrips = [];
+    // this.currentTrip = [];
   }
+
+  sortTrips() {
+    const sortedTrips = this.travelerTrips.sort((a, b) => {
+      return new Date(a.date) - new Date(b.date)
+    })
+    return sortedTrips
+  }
+
+
+
 }
 
 
--- test/Traveler-test.js
+++ test/Traveler-test.js
@@ -4,24 +4,122 @@ import Traveler from '../src/Traveler';
 
 describe('Traveler', () => {
 
-  let travelerData, travelerTrips, traveler;
+  let travelerData, travelerTrips, traveler, sortedTravelerTrips;
 
   beforeEach(function() {
 
-    travelerData = {
-        id: 1,
-        name: "Ham Leadbeater",
-        travelerType: "relaxer",
-    };
+    travelerData = {id: 2, name: "Rachael Vaughten", travelerType: "thrill-seeker"}
 
     travelerTrips = [
-        {id: 117, 
-        userID: 1, 
-        destinationID: 28, 
-        travelers: 3, 
-        date: "2021/01/09", 
-        duration: 15, 
-        status: "approved", 
+      {id: 89,
+      userID: 2,
+      destinationID: 10,
+      travelers: 5,
+      date: "2019/09/27",
+      duration: 13,
+      status: "approved",
+      suggestedActivities: []
+      },
+      {id: 100,
+      userID: 2,
+      destinationID: 6,
+      travelers: 6,
+      date: "2020/3/28",
+      duration: 10,
+      status: "approved",
+      suggestedActivities: []
+      },
+      {id: 116,
+      userID: 2,
+      destinationID: 7,
+      travelers: 3,
+      date: "2020/04/03",
+      duration: 8,
+      status: "approved",
+      suggestedActivities: []
+      },
+      {id: 166,
+      userID: 2,
+      destinationID: 7,
+      travelers: 2,
+      date: "2020/03/05",
+      duration: 6,
+      status: "approved",
+      suggestedActivities: []
+      },
+      {id: 171,
+      userID: 2,
+      destinationID: 43,
+      travelers: 1,
+      date: "2020/12/27",
+      duration: 18,
+      status: "pending",
+      suggestedActivities: []
+      },
+      {id: 177,
+      userID: 2,
+      destinationID: 20,
+      travelers: 6,
+      date: "2020/01/29",
+      duration: 8,
+      status: "approved",
+      suggestedActivities: []}
+      ];
+
+
+      sortedTravelerTrips = [
+        {id: 89,
+        userID: 2,
+        destinationID: 10,
+        travelers: 5,
+        date: "2019/09/27",
+        duration: 13,
+        status: "approved",
+        suggestedActivities: []
+        },
+        {id: 177,
+          userID: 2,
+          destinationID: 20,
+          travelers: 6,
+          date: "2020/01/29",
+          duration: 8,
+          status: "approved",
+          suggestedActivities: []
+        },
+        {id: 166,
+          userID: 2,
+          destinationID: 7,
+          travelers: 2,
+          date: "2020/03/05",
+          duration: 6,
+          status: "approved",
+          suggestedActivities: []
+        },
+        {id: 100,
+        userID: 2,
+        destinationID: 6,
+        travelers: 6,
+        date: "2020/3/28",
+        duration: 10,
+        status: "approved",
+        suggestedActivities: []
+        },
+        {id: 116,
+        userID: 2,
+        destinationID: 7,
+        travelers: 3,
+        date: "2020/04/03",
+        duration: 8,
+        status: "approved",
+        suggestedActivities: []
+        },
+        {id: 171,
+        userID: 2,
+        destinationID: 43,
+        travelers: 1,
+        date: "2020/12/27",
+        duration: 18,
+        status: "pending",
         suggestedActivities: []}
       ];
     
@@ -38,17 +136,17 @@ describe('Traveler', () => {
   });
 
   it('should keep track of a traveler\'s id', function () {
-    expect(traveler.id).to.equal(1)
+    expect(traveler.id).to.equal(2)
   });
 
   it('should keep track of a traveler\'s name', function () {
-    expect(traveler.name).to.equal("Ham Leadbeater")
+    expect(traveler.name).to.equal("Rachael Vaughten")
     traveler.name = "";
     expect(traveler.name).to.equal("")
   });
 
   it('should keep track of a traveler\'s type', function () {
-    expect(traveler.travelerType).to.equal("relaxer")
+    expect(traveler.travelerType).to.equal("thrill-seeker")
   });
 
   it('should keep track of a traveler\'s trips', function () {
@@ -57,7 +155,10 @@ describe('Traveler', () => {
     expect(traveler.travelerTrips).to.eql([]);
   });
 
-
+  it('should be able to sort a traveler\'s trips', function () {
+    expect(traveler.travelerTrips).to.equal(travelerTrips)
+    expect(traveler.sortTrips()).to.eql(sortedTravelerTrips)
+  });
 
 
 
