5a9297b89e8ff43dc80b687d12f46016580fe54e
Madison Law <madisonlaw1@gmail.com>
2022-03-05 10:36:41 -0700

    Refactor gatekeeper function to be easier to read

--- src/scripts.js
+++ src/scripts.js
@@ -7,40 +7,40 @@ import TravelDatabase from './TravelDatabase';
 
 //Selector Variables -------------------------------------------------------------------------------------
 
-const bookTripBtn = document.getElementById('bookNowButton')
-const backToMainBtn = document.getElementById('backToMain')
-const quoteBtn = document.getElementById('quoteButton')
-const clearFormBtn = document.getElementById('clearFormButton')
-const tripSubmitBtn = document.getElementById('submitButton')
+const bookTripBtn = document.getElementById('bookNowButton');
+const backToMainBtn = document.getElementById('backToMain');
+const quoteBtn = document.getElementById('quoteButton');
+const clearFormBtn = document.getElementById('clearFormButton');
+const tripSubmitBtn = document.getElementById('submitButton');
 const allTripsContainer = document.getElementById('allTrips');
 const upcomingTripsContainer = document.getElementById('upcomingTrips');
 const pendingTripsContainer = document.getElementById('pendingTrips');
-const newTripForm = document.getElementById('newTripForm')
+const newTripForm = document.getElementById('newTripForm');
 const formDepartureDate = document.getElementById('departureDate');
-const dateErrorMsg = document.getElementById('dateError')
-const formErrorTag = document.getElementById('formErrors')
+const dateErrorMsg = document.getElementById('dateError');
+const formErrorTag = document.getElementById('formErrors');
 const formTripDuration = document.getElementById('tripDuration');
 const formNumTravelers = document.getElementById('numTravelers');
 const destinationDropDown = document.getElementById('tripDestination');
-const tripQuote = document.getElementById('tripQuote')
-const filterBtnContainer = document.getElementById('tripFilterContainer')
+const tripQuote = document.getElementById('tripQuote');
+const filterBtnContainer = document.getElementById('tripFilterContainer');
 
 //Event Listeners -------------------------------------------------------------------------------------
 
-window.addEventListener('load', displayDashboard)
+window.addEventListener('load', displayDashboard);
 filterBtnContainer.addEventListener('click', displayDashboard);
 
 bookTripBtn.addEventListener('click', displayAndHideTripForm);
 backToMainBtn.addEventListener('click', displayAndHideTripForm);
 
-quoteBtn.addEventListener('click', displayTripQuote)
-clearFormBtn.addEventListener('click', clearForm)
-newTripForm.addEventListener('submit', packageNewTrip)
+quoteBtn.addEventListener('click', displayTripQuote);
+clearFormBtn.addEventListener('click', clearForm);
+newTripForm.addEventListener('submit', packageNewTrip);
 
 // Main Functions -------------------------------------------------------------------------------------------
 
 function displayDashboard(e) {
-  createDashboardView(33, e)
+  createDashboardView(33, e);
 };
 
 function createDashboardView(id, e) {
@@ -49,37 +49,46 @@ function createDashboardView(id, e) {
     createTraveler(travelDatabase, id);
     displayTravelerProfile(travelDatabase);
     createDestinationList(travelDatabase);
-    filterBtnGatekeeper(e, travelDatabase)
+    filterBtnGatekeeper(e, travelDatabase);
   });
 };
 
 function filterBtnGatekeeper(e, data) {
   if (e.target.id === 'allTripsButton') {
-    displayAllTravelerTrips(data, allTripsContainer, data.currentTraveler.travelerTrips);
-    domUpdates.showItem(allTripsContainer);
-    domUpdates.hideItem(upcomingTripsContainer);
-    domUpdates.hideItem(pendingTripsContainer);
+    filterAllTravelerTrips(data, allTripsContainer, data.currentTraveler.travelerTrips);
   } else if (e.target.id === 'upcomingTripsButton') {
-    displayAllTravelerTrips(data, upcomingTripsContainer, data.currentTraveler.upcomingTrips);
-    domUpdates.showItem(upcomingTripsContainer);
-    domUpdates.hideItem(allTripsContainer);
-    domUpdates.hideItem(pendingTripsContainer);
+    filterUpcomingTravelerTrips(data, upcomingTripsContainer, data.currentTraveler.upcomingTrips);
   } else if (e.target.id === 'pendingTripsButton') {
-    displayAllTravelerTrips(data, pendingTripsContainer, data.currentTraveler.pendingTrips);
-    domUpdates.showItem(pendingTripsContainer);
-    domUpdates.hideItem(allTripsContainer);
-    domUpdates.hideItem(upcomingTripsContainer);
+    filterPendingTravelerTrips(data, pendingTripsContainer, data.currentTraveler.pendingTrips);
   } else {
-    displayAllTravelerTrips(data, allTripsContainer, data.currentTraveler.travelerTrips);
-    domUpdates.showItem(allTripsContainer);
-    domUpdates.hideItem(upcomingTripsContainer);
-    domUpdates.hideItem(pendingTripsContainer);
+    filterAllTravelerTrips(data, allTripsContainer, data.currentTraveler.travelerTrips);
   };
 };
 
+function filterAllTravelerTrips(data, selector, arr) {
+  displayAllTravelerTrips(data, selector, arr);
+  domUpdates.showItem(allTripsContainer);
+  domUpdates.hideItem(upcomingTripsContainer);
+  domUpdates.hideItem(pendingTripsContainer);
+};
+
+function filterUpcomingTravelerTrips(data, selector, arr) {
+  displayAllTravelerTrips(data, selector, arr);
+  domUpdates.showItem(upcomingTripsContainer);
+  domUpdates.hideItem(allTripsContainer);
+  domUpdates.hideItem(pendingTripsContainer);
+};
+
+function filterPendingTravelerTrips(data, selector, arr) {
+  displayAllTravelerTrips(data, selector, arr);
+  domUpdates.showItem(pendingTripsContainer);
+  domUpdates.hideItem(allTripsContainer);
+  domUpdates.hideItem(upcomingTripsContainer);
+};
+
 function displayTravelerProfile(data) {
-  displayTravelerData(data)
-  displayTravelerSpending(data)
+  displayTravelerData(data);
+  displayTravelerSpending(data);
 };
 
 
@@ -91,57 +100,60 @@ function createTraveler(data, id) {
   newTraveler.findPendingTrips();
   newTraveler.findUpcomingTrips();
   newTraveler.findCurrentTrip();
-  domUpdates.addTravelerIDToForm(id)
+  domUpdates.addTravelerIDToForm(id);
   return newTraveler
 };
 
 function displayTravelerSpending(data) {
-  const yearlyCost = data.currentTraveler.calculateYearlyTripCost()
-  domUpdates.updateTravelerSpending(data, yearlyCost)
+  const yearlyCost = data.currentTraveler.calculateYearlyTripCost();
+  domUpdates.updateTravelerSpending(data, yearlyCost);
 };
 
 function displayTravelerData(data) {
-  domUpdates.updateWelcomeMessage(data)
-  domUpdates.updateTravelerProfile(data)
+  domUpdates.updateWelcomeMessage(data);
+  domUpdates.updateTravelerProfile(data);
 };
+
+
 
 // Filter trips  -------------------------------------------------------------------------------------------
 
 function displayAllTravelerTrips(data, selector, arr) {
   domUpdates.hideNotFoundMessage();
   if (!arr.length) {
-    domUpdates.displayNotFoundMessage()
+    domUpdates.displayNotFoundMessage();
   } else {
     selector.innerHTML = ""
     arr.forEach(trip => {
-      domUpdates.updateTravelerTrips(trip, selector)
+      domUpdates.updateTravelerTrips(trip, selector);
+      console.log(trip.date)
     });
-  }
+  };
 };
 
 // Form Page  -------------------------------------------------------------------------------------------
 
 function displayAndHideTripForm() {
-  domUpdates.displayAndHideFormPage()
+  domUpdates.displayAndHideFormPage();
 }; 
 
 function createDestinationList(data) {
   destinationDropDown.innerHTML = "";
   data.destinations.forEach(destination => {
-    domUpdates.displayDestinationDropDown(destination)
-  })
-}
+    domUpdates.displayDestinationDropDown(destination);
+  });
+};
 
 function displayTripQuote(e) {
-  e.preventDefault()
+  e.preventDefault();
   if (formDepartureDate.value < new Date().toISOString().split('T')[0]) {
-    domUpdates.displayDateError()
+    domUpdates.displayDateError();
   } else if (formNumTravelers.value <= 0 || !formNumTravelers.value || formTripDuration.value <= 0 || !formTripDuration.value) {
-    domUpdates.displayFormError()
+    domUpdates.displayFormError();
   } else {
-    handleTripQuote()
-  }
-}
+    handleTripQuote();
+  };
+};
 
 function handleTripQuote() {
   domUpdates.showItem(tripSubmitBtn);
@@ -149,36 +161,33 @@ function handleTripQuote() {
   domUpdates.hideItem(quoteBtn);
   dateErrorMsg.innerText = "";
   formErrorTag.innerText = "";
-  domUpdates.displayCostEstimate(getNewTripCost())
-  domUpdates.showItem(clearFormBtn)
-}
+  domUpdates.displayCostEstimate(getNewTripCost());
+  domUpdates.showItem(clearFormBtn);
+};
 
 function getNewTripCost() {
-  const lodgingPerDay = parseInt(destinationDropDown.options[destinationDropDown.selectedIndex].dataset.lodging)
-  const flightPerPerson = parseInt(destinationDropDown.options[destinationDropDown.selectedIndex].dataset.flight)
-
-  const lodgingTotal = lodgingPerDay * parseInt(formTripDuration.value)
-  const flightTotal = (flightPerPerson * 2) * parseInt(formNumTravelers.value)
-
-  const baseTotal = lodgingTotal + flightTotal
-  const finalTripQuote = baseTotal + (baseTotal * .10)
-
+  const lodgingPerDay = parseInt(destinationDropDown.options[destinationDropDown.selectedIndex].dataset.lodging);
+  const flightPerPerson = parseInt(destinationDropDown.options[destinationDropDown.selectedIndex].dataset.flight);
+  const lodgingTotal = lodgingPerDay * parseInt(formTripDuration.value);
+  const flightTotal = (flightPerPerson * 2) * parseInt(formNumTravelers.value);
+  const baseTotal = lodgingTotal + flightTotal;
+  const finalTripQuote = baseTotal + (baseTotal * .10);
   return finalTripQuote
-}
+};
 
 function displayTripRequestSuccess() {
   domUpdates.hideItem(tripSubmitBtn);
-  domUpdates.hideItem(tripQuote)
-  newTripForm.reset()
-  domUpdates.displayFormSuccessMsg()
-}
+  domUpdates.hideItem(tripQuote);
+  newTripForm.reset();
+  domUpdates.displayFormSuccessMsg();
+};
 
 function clearForm() {
-  newTripForm.reset()
+  newTripForm.reset();
   domUpdates.hideItem(tripSubmitBtn);
   domUpdates.hideItem(tripQuote);
   domUpdates.showItem(quoteBtn);
-}
+};
 
 //API & Promise/Error Handling ------------------------------------------------------------------------------------------------
 
@@ -193,7 +202,7 @@ function resolvePromise() {
       allData.allTrips = data[1].trips;
       allData.allDestinations = data[2].destinations;
       return allData
-    })
+    });
 };
 
 function packageNewTrip(e) {
@@ -209,23 +218,23 @@ function packageNewTrip(e) {
     suggestedActivities: []
   };
   fetchCalls.postData('trips', newTripData);
-  displayTripRequestSuccess()
+  displayTripRequestSuccess();
 }
 
 function handleServerErrors(error) {
   if (error.message === "Failed to fetch") {
-    window.alert("Oops! Something went wrong.")
-  }
-}
+    window.alert("Oops! Something went wrong.");
+  };
+};
 
 function checkForErrors(response) {
   if (response.ok) {
     formErrorTag.innerText = "";
-    return response.json()
+    return response.json();
   } else {
-    throw new Error("Make sure you fill out all form fields!")
-  }
-}
+    throw new Error("Make sure you fill out all form fields!");
+  };
+};
 
 
 
