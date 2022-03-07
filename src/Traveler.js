466b35c049a7107341f8eb0f89b1c63771585779
Madison Law <madisonlaw1@gmail.com>
2022-03-07 13:33:05 -0700

    Clean up of Traveler class

--- src/Traveler.js
+++ src/Traveler.js
@@ -1,4 +1,3 @@
-
 class Traveler {
   constructor(travelerData, travelerTrips, destinations) {
     this.id = travelerData.id;
@@ -10,14 +9,14 @@ class Traveler {
     this.currentTrip = [];
     this.pendingTrips = [];
     this.destinations = destinations;
-  }
+  };
 
   sortTrips() {
     const sortedTrips = this.travelerTrips.sort((a, b) => {
-      return new Date(a.date) - new Date(b.date)
-    })
-    return sortedTrips
-  }
+      return new Date(a.date) - new Date(b.date);
+    });
+    return sortedTrips;
+  };
 
   findPastTrips() {
     this.sortTrips();
@@ -25,9 +24,9 @@ class Traveler {
     this.pastTrips = this.travelerTrips.filter(trip => {
       let departureDate = new Date(trip.date);
       let returnDate = new Date(departureDate.setDate(departureDate.getDate() + trip.duration));
-      return new Date(trip.date) < new Date(currentDate) && returnDate < new Date(currentDate)
+      return new Date(trip.date) < new Date(currentDate) && returnDate < new Date(currentDate);
     });
-    return this.pastTrips
+    return this.pastTrips;
   };
 
   findUpcomingTrips() {
@@ -36,9 +35,9 @@ class Traveler {
     this.upcomingTrips = this.travelerTrips.filter(trip => {
       let departureDate = new Date(trip.date);
       let returnDate = new Date(departureDate.setDate(departureDate.getDate() + trip.duration));
-      return new Date(trip.date) > new Date(currentDate) && returnDate > new Date(currentDate)
-    })
-    return this.upcomingTrips
+      return new Date(trip.date) > new Date(currentDate) && returnDate > new Date(currentDate);
+    });
+    return this.upcomingTrips;
   };
 
   findCurrentTrip() {
@@ -47,24 +46,24 @@ class Traveler {
     this.currentTrip = this.travelerTrips.filter(trip => {
       let departureDate = new Date(trip.date);
       let returnDate = new Date(departureDate.setDate(departureDate.getDate() + trip.duration));
-      return new Date(trip.date) <= new Date(currentDate) && returnDate >= new Date(currentDate)
+      return new Date(trip.date) <= new Date(currentDate) && returnDate >= new Date(currentDate);
     })
-    return this.currentTrip
+    return this.currentTrip;
   };
 
   findPendingTrips() {
     this.sortTrips();
     this.pendingTrips = this.travelerTrips.filter(trip => trip.status === "pending");
-    return this.pendingTrips
+    return this.pendingTrips;
   };
 
   findTravelerDestinations() {
-    this.sortTrips()
+    this.sortTrips();
     this.travelerTrips.map(trip => {
       const matchingDestination = this.destinations.find(destination => destination.id === trip.destinationID);
-      return trip.destinationID = matchingDestination
-    })
-    return this.travelerTrips
+      return trip.destinationID = matchingDestination;
+    });
+    return this.travelerTrips;
   };
 
   findYearlyTrips() {
@@ -73,10 +72,10 @@ class Traveler {
     const currentYear = new Date().getFullYear();
     this.travelerTrips.forEach(trip => {
       if (new Date(trip.date).getFullYear() === currentYear) {
-        thisYearsTrips.push(trip)
-      }
+        thisYearsTrips.push(trip);
+      };
     })
-    return thisYearsTrips
+    return thisYearsTrips;
   };
 
   calculateYearlyTripCost() {
@@ -90,18 +89,15 @@ class Traveler {
     const baseCost = lodgingCost + flightCost;
     const travelAgentFee = baseCost / 10;
     const finalTotal = baseCost + travelAgentFee;
-
     return finalTotal;
   };
 
   findNumDaysTraveled() {
     const approvedTrips = this.travelerTrips.filter(trip => trip.status === "approved");
-    
     return approvedTrips.reduce((total, trip) => {
-      return total += trip.duration
+      return total += trip.duration;
     }, 0)
   };
-
 };
 
 
