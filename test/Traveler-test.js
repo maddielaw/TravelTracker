7ba3aa7cd668498ecbad103cc617f7169c80f9e3
Madison Law <madisonlaw1@gmail.com>
2022-03-08 09:26:21 -0700

    Streamline Traveler-test

--- test/Traveler-test.js
+++ test/Traveler-test.js
@@ -204,7 +204,7 @@ describe('Traveler', () => {
 
   it('should be able to find a traveler\'s past trips', function () {
     traveler.findPastTrips();
-    expect(traveler.pastTrips).to.eql([
+    expect(traveler.pastTrips[0]).to.eql(
       {id: 89,
         userID: 2,
         destinationID: 10,
@@ -213,39 +213,12 @@ describe('Traveler', () => {
         duration: 13,
         status: "approved",
         suggestedActivities: []
-        },
-        {id: 177,
-        userID: 2,
-        destinationID: 20,
-        travelers: 6,
-        date: "2020/01/29",
-        duration: 8,
-        status: "approved",
-        suggestedActivities: []
-        },
-        {id: 166,
-        userID: 2,
-        destinationID: 7,
-        travelers: 2,
-        date: "2020/03/05",
-        duration: 6,
-        status: "approved",
-        suggestedActivities: []
-        },
-        {id: 100,
-        userID: 2,
-        destinationID: 6,
-        travelers: 6,
-        date: "2020/3/28",
-        duration: 10,
-        status: "approved",
-        suggestedActivities: []
-        }]);
+        });
   });
 
   it('should be able to find a traveler\'s upcoming trips', function () {
     traveler.findUpcomingTrips();
-    expect(traveler.upcomingTrips).to.eql([
+    expect(traveler.upcomingTrips[0]).to.eql(
       {id: 116,
         userID: 2,
         destinationID: 7,
@@ -254,15 +227,7 @@ describe('Traveler', () => {
         duration: 8,
         status: "approved",
         suggestedActivities: []
-        },
-        {id: 171,
-        userID: 2,
-        destinationID: 43,
-        travelers: 1,
-        date: "2022/12/27",
-        duration: 18,
-        status: "pending",
-        suggestedActivities: []}])
+        })
   });
 
   it('should be able to find a traveler\'s current trip(s)', function () {
@@ -284,8 +249,9 @@ describe('Traveler', () => {
     ]);
   });
 
-  it('should be able to find a traveler\'s destinations', function () {
-    expect(traveler.findTravelerDestinations()).to.eql([
+  it('should be able to find a traveler\'s destinations and add them to the traveler\'s trips', function () {
+    traveler.findTravelerDestinations();
+    expect(traveler.travelerTrips[0]).to.eql(
       {
         id: 89,
         userID: 2,
@@ -294,99 +260,13 @@ describe('Traveler', () => {
           destination: 'Toronto, Canada',
           estimatedLodgingCostPerDay: 90,
           estimatedFlightCostPerPerson: 450,
-          image: 'https://images.unsplash.com/photo-1535776142635-8fa180c46af7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2756&q=80'
-        },
+          image: 'https://images.unsplash.com/photo-1535776142635-8fa180c46af7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2756&q=80'},
         travelers: 5,
         date: '2019/09/27',
         duration: 13,
         status: 'approved',
         suggestedActivities: []
-      },
-      {
-        id: 177,
-        userID: 2,
-        destinationID: {
-          id: 20,
-          destination: 'Miami, Florida',
-          estimatedLodgingCostPerDay: 158,
-          estimatedFlightCostPerPerson: 275,
-          image: 'https://images.unsplash.com/photo-1514214246283-d427a95c5d2f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1573&q=80',
-          alt: 'sand with palm trees and tall buildings in the background'
-        },
-        travelers: 6,
-        date: '2020/01/29',
-        duration: 8,
-        status: 'approved',
-        suggestedActivities: []
-      },
-      {
-        id: 166,
-        userID: 2,
-        destinationID: {
-          id: 7,
-          destination: 'Paris, France',
-          estimatedLodgingCostPerDay: 100,
-          estimatedFlightCostPerPerson: 395,
-          image: 'https://images.unsplash.com/photo-1524396309943-e03f5249f002?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1567&q=80',
-          alt: 'city during the day time with eiffel tower'
-        },
-        travelers: 2,
-        date: '2020/03/05',
-        duration: 6,
-        status: 'approved',
-        suggestedActivities: []
-      },
-      {
-        id: 100,
-        userID: 2,
-        destinationID: {
-          id: 6,
-          destination: 'Jakarta, Indonesia',
-          estimatedLodgingCostPerDay: 70,
-          estimatedFlightCostPerPerson: 890,
-          image: 'https://images.unsplash.com/photo-1555333145-4acf190da336?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
-          alt: 'lit up city at night'
-        },
-        travelers: 6,
-        date: '2020/3/28',
-        duration: 10,
-        status: 'approved',
-        suggestedActivities: []
-      },
-      {
-        id: 116,
-        userID: 2,
-        destinationID: {
-          id: 7,
-          destination: 'Paris, France',
-          estimatedLodgingCostPerDay: 100,
-          estimatedFlightCostPerPerson: 395,
-          image: 'https://images.unsplash.com/photo-1524396309943-e03f5249f002?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1567&q=80',
-          alt: 'city during the day time with eiffel tower'
-        },
-        travelers: 3,
-        date: '2022/04/03',
-        duration: 8,
-        status: 'approved',
-        suggestedActivities: []
-      },
-      {
-        id: 171,
-        userID: 2,
-        destinationID: {
-          id: 43,
-          destination: 'Nassau, The Bahamas',
-          estimatedLodgingCostPerDay: 550,
-          estimatedFlightCostPerPerson: 90,
-          image: 'https://images.unsplash.com/photo-1548574505-5e239809ee19?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1664&q=80',
-          alt: 'aerial photography of white and blue cruise ships during daytime'
-        },
-        travelers: 1,
-        date: '2022/12/27',
-        duration: 18,
-        status: 'pending',
-        suggestedActivities: []
-      }]);
+      });
     });
       
   it('should be able to find a traveler\'s trips for the current year', function () {
